bd818e560899ceab1294d346c7a872899f61f0a7
Rob Martin <martinrobd@gmail.com>
2019-06-16 23:11:51 -0500

    Testing VS Code integration

--- 41/login.py
+++ 41/login.py
@@ -1,4 +1,5 @@
-# If you worked with Flask or Django you must have seen routes being decorated to enforce authentication.
+# If you worked with Flask or Django you must have seen routes being decorated
+# to enforce authentication.
 
 # In this Bite you will write your own login checking decorator.
 
@@ -16,32 +17,33 @@
 
 # user is not on the system, return "please create an account"
 # user is on the system but not logged in, return "please login"
-# user is on the system and logged in, return the function's "welcome back {user}"
+# user is on the system and logged in, return the function's
+# "welcome back {user}"
 # See also the tests for more details. Have fun and enjoy!
 
 from functools import wraps
 
 
-known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
-loggedin_users = ['mike', 'sue']
+known_users = ["bob", "julian", "mike", "carmen", "sue"]
+loggedin_users = ["mike", "sue"]
 
 
 def login_required(func):
     @wraps(func)
     def wrapper(*args):
-    	# user is not on the system, return "please create an account"
-    	if args[0] not in known_users:
-    		return "please create an account"
+        # user is not on the system, return "please create an account"
+        if args[0] not in known_users:
+            return "please create an account"
 
-    	# user is on the system but not logged in, return "please login"
-    	if args[0] not in loggedin_users:
-    		return "please login"
+        # user is on the system but not logged in, return "please login"
+        if args[0] not in loggedin_users:
+            return "please login"
+        return func(*args)
 
-    	return func(*args)
     return wrapper
 
 
 @login_required
 def welcome(user):
-    '''Return a welcome message if logged in'''
-    return f'welcome back {user}'
+    """Return a welcome message if logged in"""
+    return f"welcome back {user}"
